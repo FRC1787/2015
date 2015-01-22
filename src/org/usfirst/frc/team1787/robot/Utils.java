ab0e674d64811f34a519f75eaa97749d4bdd40f9
mag9 <razestudiosdev@gmail.com>
2015-01-22 17:03:32 -0500

    add utils

---
+++ src/org/usfirst/frc/team1787/robot/Utils.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1787.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+/**
+ * Various utilities.
+ * @author jeremystark & ryan
+ *
+ */
+public class Utils
+{
+	
+	
+	/**
+	 * Print text to the Driver's Station.
+	 * @param text The text to print.
+	 */
+	public static void print(String text)
+	{
+		DriverStation.reportError(text + "\n", false);
+	}
+
+}
